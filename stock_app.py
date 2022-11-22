961349ab7a5cbf21b91919fd5a76ffdf84d2f8bb
Stefanie <stefcodes@gmail.com>
2022-11-21 17:39:20 -0800

    add volume and closing price

--- stock_app.py
+++ stock_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.write("""
  # Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing price** and **volume** of Google!
 
 """
 )
@@ -16,5 +16,13 @@ tickerdata = yf.Ticker(tickersymbol)
 
 tickerDf = tickerdata.history(period='id', start='2020-5-03', end='2022-5-31')
 
+
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
